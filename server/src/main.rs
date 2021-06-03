31cc8eef1b09fb963852eafc74e85dc07a46de44
ZaneStaufer <stauffer_zane@columbusstate.edu>
2021-06-03 18:37:16 -0400

    Database container

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,7 @@
 use warp::*;
 use std::collections::HashMap;
 use serde::{Serialize, Deserialize};
-
-type Items = HashMap<String, i32>;
+use toml::{de::Error};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 struct Item {
@@ -12,15 +11,33 @@ struct Item {
 
 #[tokio::main]
 async fn main(){
+    let holomon_config: HashMap<String, String>;
+    match read_config(){
+        Ok(c) => {
+            holomon_config = c;
+        },
+        Err(_) => {
+            panic!("There was a problem loading the config.");
+        }
+    };
+
     let mut res = HashMap::new();
     res.insert("test", Item{name: "testaaaa".to_string(), quantity: 20});
 
-    let hello = warp::path!("hello" / String)
-        .map(move |name| {
+    
+
+    let hello = warp::path!("hello")
+        .map(move || {
             warp::reply::json(&res)
         });
     
     warp::serve(hello)
         .run(([0, 0, 0, 0], 3030))
         .await;
+}
+
+fn read_config() -> Result<HashMap<String, String>, Error>{
+    let mut config = config::Config::default();
+    config.merge(config::File::with_name("Config")).unwrap();
+    Ok(config.try_into::<HashMap<String, String>>().unwrap())
 }
